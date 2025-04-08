7620cba2de04ec9de1456dadfb11f2605c1b3d47
Fast4x <rinorusso@gmail.com>
2025-04-08 23:39:28 +0200

    feat: enable fullscreebìn and make status and navigation bars transparent

--- app/src/main/java/it/fast4x/rilauncher/MainActivity.kt
+++ app/src/main/java/it/fast4x/rilauncher/MainActivity.kt
@@ -5,14 +5,21 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
+import androidx.core.view.WindowCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.WindowInsetsControllerCompat
 import it.fast4x.rilauncher.ui.home.HomeScreen
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-
+        val insetsController = WindowCompat.getInsetsController(window, window.decorView)
+        insetsController.apply {
+            hide(WindowInsetsCompat.Type.statusBars())
+            hide(WindowInsetsCompat.Type.navigationBars())
+            systemBarsBehavior = WindowInsetsControllerCompat.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
+        }
         setContent {
             HomeScreen()
         }
